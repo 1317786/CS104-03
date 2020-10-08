b4ceb8b3e72c1b6e035e21d32198339356330962
1317786 <71734110+1317786@users.noreply.github.com>
2020-10-08 14:53:40 -0400

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,144 @@
+
+temp = int(input("Please enter the current temperature: "))
+
+if temp > 90:
+    print("Wear Shorts")
+elif temp > 70:
+    print("Short sleeves are fine")
+elif temp > 50:
+    print("Wear a jacket")
+elif temp > 32:
+    print("Wear a heavy coat")
+else:
+    print("Stay inside")
+
+again = input("Would you like to enter another temperature? enter y/n: ")
+if again == "n":
+    print ("Thank you for testing this out!")
+    quit
+
+    if again == "y":
+        print ("Please enter the current temperature: ")
+
+
+        
+temp = int(input("Please enter the current temperature: "))
+
+if temp > 90:
+    print("Wear Shorts")
+elif temp > 70:
+    print("Short sleeves are fine")
+elif temp > 50:
+    print("Wear a jacket")
+elif temp > 32:
+    print("Wear a heavy coat")
+else:
+    print("Stay inside")
+
+    
+again = input("Would you like to enter another temperature? enter y/n: ")
+if again == "n":
+    print ("Thank you for testing this out!")
+    quit
+
+    if again == "y":
+        print ("Please enter the current temperature: ")
+        
+     
+
+
+temp = int(input("Please enter the current temperature: "))
+
+if temp > 90:
+    print("Wear Shorts")
+elif temp > 70:
+    print("Short sleeves are fine")
+elif temp > 50:
+    print("Wear a jacket")
+elif temp > 32:
+    print("Wear a heavy coat")
+else:
+    print("Stay inside")
+
+
+again = input("Would you like to enter another temperature? enter y/n: ")
+if again == "n":
+    print ("Thank you for testing this out!")
+    quit
+
+    if again == "y":
+        print ("Please enter the current temperature: ")
+
+
+
+
+
+temp = int(input("Please enter the current temperature: "))
+
+if temp > 90:
+    print("Wear Shorts")
+elif temp > 70:
+    print("Short sleeves are fine")
+elif temp > 50:
+    print("Wear a jacket")
+elif temp > 32:
+    print("Wear a heavy coat")
+else:
+    print("Stay inside")
+
+
+
+
+again = input("Would you like to enter another temperature? enter y/n: ")
+if again == "n":
+    print ("Thank you for testing this out!")
+    quit
+
+    if again == "y":
+        print ("Please enter the current temperature: ")
+
+
+
+
+
+temp = int(input("Please enter the current temperature: "))
+
+if temp > 90:
+    print("Wear Shorts")
+elif temp > 70:
+    print("Short sleeves are fine")
+elif temp > 50:
+    print("Wear a jacket")
+elif temp > 32:
+    print("Wear a heavy coat")
+else:
+    print("Stay inside")
+
+
+
+
+
+
+
+again = input("There are no further options, thank you for testing out this py program, please enter n to finish. enter n: ")
+if again == "n":
+    print ("Thank you for testing out this program and have a wonderful rest of the day!")
+    quit
+
+    if again == "y":
+        print ("Please enter the current temperature: ")
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+        
